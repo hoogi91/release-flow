2c0374fe68b377f158fe540f28e0b3149f2a0198
Thorsten.Hogenkamp <thorsten.hogenkamp@udg.de>
2019-01-23 19:55:40 +0100

    enable phpunit execution and code coverage reports

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,8 +1,2 @@
 <?php
-/**
- * Manual test
- */
-require_once dirname(__DIR__) .'/vendor/autoload.php';
-
-$application = new Hoogi91\ReleaseFlow\Application('release-flow TEST');
-$application->run();
+require_once dirname(__DIR__) . '/vendor/autoload.php';
